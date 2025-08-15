5f4848b1da9fce70cc765cf84f377a6636cdcd72
Vaidehi dubey <vaidehidubey122005@users.noreply.github.com>
2025-08-15 17:05:16 +0530

    changes

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,33 +1,40 @@
-'use client'
+import { userDetailContext } from '@/context/UserDetailContext';
 import { api } from '@/convex/_generated/api';
 import { useUser } from '@clerk/nextjs';
 import { useMutation } from 'convex/react';
-import React, { useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react';
 
-interface ProviderProps {
-  children: React.ReactNode;
-}
 
-const Provider = ({ children }: ProviderProps) => {
+function Provider({ children }: any) {
+
     const { user } = useUser();
     const CreateUser = useMutation(api.users.CreateNewUser);
+    const [userDetail, setUserDetail] = useState<any>(undefined);
+    console.log('user')
+    useEffect(() => {
+
+        user && CreateNewUser();
+    }, [user])
 
     const CreateNewUser = async () => {
-        const result = await CreateUser({
-            email: user?.primaryEmailAddress?.emailAddress ?? '',
-            imageUrl: user?.imageUrl ?? '',
-            name: user?.fullName ?? ''
-        });
-        console.log("User created or fetched:", result);
-    };
-
-    useEffect(() => {
-        if (user) CreateNewUser();
-    }, [user]);
+        if (user) {
+            const result = await CreateUser({
+                email: user?.primaryEmailAddress?.emailAddress ?? '',
+                imageUrl: user?.imageUrl,
+                name: user?.fullName ?? ''
+            });
+            console.log(result)
+        setUserDetail(result);
+    }
+}
 
     return (
-        <div>{children}</div>
+        <userDetailContext.Provider value={{ userDetail, setUserDetail }}>
+            <div>{children}</div>
+        </userDetailContext.Provider>
     );
-};
+}
 
-export default Provider
+export default Provider;
+
+export const useUserDetailContext = () => useContext(userDetailContext);
